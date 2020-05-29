823aa0a7791fddaf7ff3877c69eb25567547333d
Irina Belousova <irinabelousova11@gmail.com>
2020-05-29 21:09:56 +0400

    Finished all toggle buttons

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Toggle buttons</h1>
+<p> I built some toggle buttons for practice in CSS.</p>
+
+<h3>Technologies used:</h3>
+<ul>
+<li>HTML5</li>
+<li>CSS3</li>
+<li>JavaScript</li>
+</ul>
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let checkbox2 = document.getElementById("checkbox2");
 let checkbox3 = document.getElementById("checkbox3");
 let checkbox5 = document.getElementById("checkbox5");
+let checkbox7 = document.getElementById("checkbox7");
+let checkbox8 = document.getElementById("checkbox8");
 
 checkbox2.addEventListener("click", function () {
   this.parentElement.classList.toggle("active");
@@ -11,6 +13,14 @@ checkbox3.addEventListener("click", function () {
 });
 
 checkbox5.addEventListener("click", function () {
+  this.parentElement.classList.toggle("active");
+});
+
+checkbox7.addEventListener("click", function () {
+  this.parentElement.classList.toggle("active");
+});
+
+checkbox8.addEventListener("click", function () {
   this.parentElement.classList.toggle("active");
 });
 
